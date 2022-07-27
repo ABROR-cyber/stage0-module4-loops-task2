ac665637c698a762829f3a934ccf58a3fc8648a3
abror <abror01042001@gmail.com>
2022-07-27 21:58:03 +0500

    m

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int a=1;
+        System.out.println(1);
+        for (int i = 1; i <= printToInclusive; i++) {
+            System.out.println(a*=i);
+
+        }
 
     }
 }
